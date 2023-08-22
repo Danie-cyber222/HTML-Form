3776edc0c3f975343c37fb548d170b17dd603308
Danie-cyber222 <75931875+Danie-cyber222@users.noreply.github.com>
2023-08-22 14:57:53 +0100

    Add files via upload

---
+++ javascript challenge/challenge-2.js
@@ -0,0 +1,17 @@
+ const man = document.getElementById("man");
+ var formInput = document.getElementsByClassName("formInput").value;
+ 
+ function secondValue(arr) {
+    return arr 
+ }
+ 
+ setMan = ()=>{
+        //man.innerHTML = "bad man"
+    if (formInput === "") {
+        man.innerHTML = "you don't have the facilities for that big man!!!."
+        }
+        console.log("good man");
+ }
+ console.log(secondValue([1, 2,3, 4, 5]));
+ console.log(secondValue(0))
+ console.log(secondValue(1));
